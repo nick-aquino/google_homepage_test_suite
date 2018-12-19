11b8643818d5c9bef5ada790464550aba7aa91b2
Nick Aquino <aquino.nj@gmail.com>
2018-12-19 17:57:04 -0500

    fixing typo in requirements.txt

--- pages/searchpage.py
+++ pages/searchpage.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 # Interface for Google Search Page
 # Stores page specific information in one place for ease of use and modification
 class SearchPage:
+
     page_url = "https://www.google.com/search"
     title_suffix = "Google Search"
 
